c343adb7ffa5ba82e95cb28935e3f8c1b0fa3b70
sikerbela2 <fahrican.kosar@metu.edu.tr>
2015-12-21 16:12:35 +0200

    Changes on bootstrap.py- it has minimal functionality.

--- bootstrap.py
+++ bootstrap.py
@@ -10,52 +10,73 @@ import subprocess
 
 from subprocess import PIPE
 
-file_list = glob.glob("add_category/*") +\
-            glob.glob("add_competitor/*") +\
-            glob.glob("calculate_score/*") +\
-            glob.glob("results/*") +\
-            glob.glob("style/*") +\
-            glob.glob("images/*") +\
-            ["index.html"]
+def main():
+    "Copy -> minify -> alter connect addresses."
+    file_list = glob.glob("add_category/*") +\
+                glob.glob("add_competitor/*") +\
+                glob.glob("calculate_score/*") +\
+                glob.glob("results/*") +\
+                glob.glob("style/*") +\
+                glob.glob("images/*") +\
+                ["index.html"]
 
-js_list = ["add_category/add_category.js",
-           "add_competitor/add_competitor.js",
-           "calculate_score/calculate_score.js",
-           "results/results.js"]
+    js_list = ["add_category/add_category.js",
+               "add_competitor/add_competitor.js",
+               "calculate_score/calculate_score.js",
+               "results/results.js"]
 
-print("[BOOTSTRAP] Where is your www directory? (default: /var/www/html/")
-www_directory = input()
-if www_directory == "":
-    www_directory = "/var/www/html"
+    html_list = ["add_category/index.html",
+                 "add_competitor/index.html",
+                 "calculate_score/index.html",
+                 "results/index.html"]
 
-print("[BOOTSTRAP] Copying files...")
-subprocess.call(["mkdir", "-pv", www_directory + "/Skorbela/"])
-for file_path in file_list:
-    new_path = www_directory + "/Skorbela/" + file_path
-    args = ["cp", "--parents", "-v", file_path, www_directory + "/Skorbela/"]
-    cp = subprocess.call(args)
+    print("[BOOTSTRAP] Where is your www directory? (default: /var/www/html/")
+    www_directory = input()
+    if www_directory == "":
+        www_directory = "/var/www/html"
 
-print("[BOOTSTRAP] Checking if you have UglifyJS installed...")
-try:
-    a = subprocess.Popen(["uglifyjs"])
-except FileNotFoundError:
-    print("[BOOTSTRAP] You don't have UglifyJS - please install it.")
-    sys.exit(1)
-finally:
-    a.terminate()
+    print("[BOOTSTRAP] Copying files...")
+    subprocess.call(["mkdir", "-pv", www_directory + "/Skorbela/"])
+    for file_path in file_list:
+        new_path = www_directory + "/Skorbela/" + file_path
+        args = ["cp", "--parents", "-v", file_path, www_directory + "/Skorbela/"]
+        subprocess.call(args)
 
-print("[BOOTSTRAP] Minifying JS...")
-try:
-    for js_file in js_list:
-        args = ["uglifyjs", js_file, "-m", "-c"]
-        uglifyjs = subprocess.Popen(args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        output, err = uglifyjs.communicate()
-        returncode = uglifyjs.wait()
-        if returncode != 0:
-            raise RuntimeError
-        new_path = www_directory + "/Skorbela/" + js_file
-        js_file_handle = open(new_path, 'wb')
-        js_file_handle.write(output)
-        js_file_handle.close()
-except RuntimeError:
-    print("[BOOTSTRAP] Error: UglifyJS exited with non-0 exit code.")   
+    print("[BOOTSTRAP] Checking if you have UglifyJS installed...")
+    try:
+        uglifyjs = subprocess.Popen(["uglifyjs"])
+    except FileNotFoundError:
+        print("[BOOTSTRAP] You don't have UglifyJS - please install it.")
+        sys.exit(1)
+    else:
+        uglifyjs.terminate()
+
+    print("[BOOTSTRAP] Minifying JS...")
+    try:
+        for js_file in js_list:
+            args = ["uglifyjs", js_file, "-m", "-c"]
+            uglifyjs = subprocess.Popen(args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            output, err = uglifyjs.communicate()
+            returncode = uglifyjs.wait()
+            if returncode != 0:
+                raise RuntimeError
+            new_path = www_directory + "/Skorbela/" + js_file
+            with open(new_path, 'wb') as f_handle:
+                f_handle.write(output)
+    except RuntimeError:
+        print("[BOOTSTRAP] Error: UglifyJS exited with non-0 exit code.")
+        print("[BOOTSTRAP] stderr of UglifyJS:\n" + err)
+
+    print("[BOOTSTRAP] Altering pages' onload connect addresses...")
+    websocket_address = "ws://" + socket.getfqdn() + ":31313"
+    for html_file in html_list:
+        html_file = www_directory + "/Skorbela/" + html_file
+        with open(html_file, 'r') as f_handle:
+            html_string = f_handle.read()
+        html_string.replace("onLoad=\"connect(\'ws://localhost:31313\');\"",
+                            "onLoad=\"connect(\'" + websocket_address + "\');\"")
+        with open(html_file, 'w') as f_handle:
+            f_handle.write(html_string)
+
+if __name__ == "__main__":
+    main()
